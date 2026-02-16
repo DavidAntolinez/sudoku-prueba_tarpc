61d2a6a818932663ef1d93068d3841374b8d99a3
DavidAntolinez <antolinezchavarrodavid@gmail.com>
2026-02-16 11:37:16 -0500

    rellenar sudoku

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod client;
 
 use std::io::{self, stdout};
 use std::sync::{Arc, Mutex};
-use crossterm::{event::{self, Event, KeyCode}, terminal::{enable_raw_mode, disable_raw_mode}, ExecutableCommand};
+use crossterm::{event::{self, Event, KeyCode, KeyEventKind}, terminal::{enable_raw_mode, disable_raw_mode}, ExecutableCommand};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use ratatui::{
     prelude::*,
@@ -42,6 +42,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         scroll_cliente: 0,
         scroll_server: 0,
         scroll_rpc: 0,
+
+        input_mode: false,
+        input_stage: 0,
+        input_buffer: String::new(),
+
+        input_row: None,
+        input_col: None,
+        input_value: None,
     };
 
     loop {
@@ -49,22 +57,108 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         if event::poll(std::time::Duration::from_millis(50))? {
             if let Event::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Char('1') => {
-                        let sudoku = app.client.sudoku4x4().await;
-                        app.sudoku = Some(sudoku)
-                    },
-                    KeyCode::Char('2') => {
-                        let sudoku = app.client.sudoku9x9().await;
-                        app.sudoku = Some(sudoku)
-                    },
-                    KeyCode::Char('3') => {
-                        let sudoku = app.client.sudoku16x16().await;
-                        app.sudoku = Some(sudoku)
-                    },
-                    KeyCode::Char('q') | KeyCode::Char('4') => {break;},
-                    _ => {}
+
+                // FILTRO IMPORTANTE
+                if key.kind != KeyEventKind::Press {
+                    continue;
                 }
+
+                // SI ESTAMOS EN MODO INPUT
+                if app.input_mode && let Some(_sudoku) = &app.sudoku{
+
+                    match key.code {
+
+                        KeyCode::Char(c) if c.is_ascii_digit() => {
+                            app.input_buffer.push(c);
+                        }
+
+                        KeyCode::Backspace => {
+                            app.input_buffer.pop();
+                        }
+
+                        KeyCode::Enter => {
+
+                            let value: u8 = app.input_buffer.parse().unwrap_or(0);
+
+                            match app.input_stage {
+                                0 => app.input_row = Some(value),
+                                1 => app.input_col = Some(value),
+                                2 => app.input_value = Some(value),
+                                _ => {}
+                            }
+
+                            app.input_buffer.clear();
+
+                            if app.input_stage < 2 {
+                                app.input_stage += 1;
+                            } else {
+
+                                // modificar sudoku
+
+                                if let (Some(row), Some(col), Some(val)) = (app.input_row, app.input_col, app.input_value){
+                                    if let Some(sudoku) = &mut app.sudoku{
+                                        let size = sudoku.board.len() as u8;
+
+                                        if row < size && col < size{
+
+                                            // logica de relleno
+                                            if sudoku.board[row as usize][col as usize] == 0{
+                                                sudoku.board[row as usize][col as usize] = val;
+                                            }
+                                        }
+                                    }
+                                    
+                                }
+
+                                app.input_mode = false;
+                                app.input_stage = 0;
+                            }
+                        }
+
+                        KeyCode::Esc => {
+                            app.input_mode = false;
+                            app.input_stage = 0;
+                            app.input_buffer.clear();
+                        }
+
+                        _ => {}
+                    }
+
+                } else {
+
+                    match key.code {
+
+                        KeyCode::Char('1') => {
+                            let sudoku = app.client.sudoku4x4().await;
+                            app.sudoku = Some(sudoku)
+                        },
+
+                        KeyCode::Char('2') => {
+                            let sudoku = app.client.sudoku9x9().await;
+                            app.sudoku = Some(sudoku)
+                        },
+
+                        KeyCode::Char('3') => {
+                            let sudoku = app.client.sudoku16x16().await;
+                            app.sudoku = Some(sudoku)
+                        },
+
+                        KeyCode::Char('4') => {
+                            if let Some(_sudoku) = &app.sudoku{
+                                app.input_mode = true;
+                                app.input_stage = 0;
+                                app.input_buffer.clear();
+                            }
+                        },
+
+                        KeyCode::Char('q') | KeyCode::Char('5') => {
+                            break;
+                        }
+
+                        _ => {}
+                    }
+                }
+
             }
         }
 
@@ -83,6 +177,14 @@ struct App {
     scroll_cliente: u16,
     scroll_server: u16,
     scroll_rpc: u16,
+
+    input_mode: bool,
+    input_stage: u8, // 0=fila, 1=columna, 2=valor
+    input_buffer: String,
+
+    input_row: Option<u8>,
+    input_col: Option<u8>,
+    input_value: Option<u8>,
 }
 
 
@@ -98,19 +200,28 @@ fn draw_ui(frame: &mut Frame, buffers: &LogBuffers, app: &mut App) {
     let left = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
-            Constraint::Length(10),   // menú
-            Constraint::Min(10),     // sudoku
+            Constraint::Length(10), // menú
+            Constraint::Length(3),  // input (nuevo)
+            Constraint::Min(10),    // sudoku
         ])
         .split(layout[0]);
 2;
-    let menu = Paragraph::new("1. Sudoku 4x4 \n2. Sudoku 9x9 \n3. Sudoku 16x16 \n4 o q. Salir")
-        .wrap( Wrap::default() )
+    let menu = Paragraph::new(
+            "1. Sudoku 4x4 \
+            \n2. Sudoku 9x9 \
+            \n3. Sudoku 16x16 \
+            \n4. Ingresar valor \
+            \n5 o q. Salir"
+        )
+        .wrap(Wrap::default())
         .block(Block::default().title("Menu").borders(Borders::ALL));
 
     frame.render_widget(menu, left[0]);
 
     let sudoku = sudoku_widget(app.sudoku.as_ref());
-    frame.render_widget(sudoku, left[1]);
+    frame.render_widget(sudoku, left[2]);
+
+    
 
 
     let right = Layout::default()
@@ -156,6 +267,26 @@ fn draw_ui(frame: &mut Frame, buffers: &LogBuffers, app: &mut App) {
     frame.render_widget(cliente, right[0]);
     frame.render_widget(server, right[1]);
     frame.render_widget(rpc, right[2]);
+
+    if app.input_mode {
+
+            let title = match app.input_stage {
+            0 => "Fila",
+            1 => "Columna",
+            2 => "Valor",
+            _ => "",
+        };
+
+        let input = Paragraph::new(app.input_buffer.clone())
+            .block(Block::default().title(title).borders(Borders::ALL))
+            .alignment(Alignment::Center);
+
+        frame.render_widget(input, left[1]);
+    }else {
+        let empty = Paragraph::new("")
+            .block(Block::default().borders(Borders::ALL).title("Input"));
+        frame.render_widget(empty, left[1]);
+    }
 }
 
 
@@ -191,6 +322,3 @@ fn render_board(board: &[Vec<u8>]) -> String {
 
     out
 }
-
-
-
